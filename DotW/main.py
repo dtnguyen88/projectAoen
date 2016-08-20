8c99dc3fe9e201b3ba204529f37a363f4e7c2746
dtnguyen <duc.thong.nguyen.88@gmail.com>
2016-08-21 00:05:52 +0100

    Adding groupby

--- DotW/main.py
+++ DotW/main.py
@@ -3,7 +3,7 @@ import pandas_datareader.data as web
 import datetime
 
 def classify(row):
-    if row > 0:
+    if row['Adj Close'] > 0:
         return 'Up'
     return 'Down'
 
@@ -14,8 +14,12 @@ sp500 = stockUtils.get_constitutes('sp500')
 print(sp500)
 for ticker in sp500:
     symbol = ticker['symbol']
-    prices = web.DataReader(symbol, 'yahoo', start, end)['Adj Close']
+    prices = web.DataReader(symbol, 'yahoo', start, end)
     pct_change = prices.pct_change()
     pct_change = pct_change.ix[1:]
-    pct_change['Up/Down'] = pct_change.apply(classify)
-    print(pct_change)
+    pct_change['Up/Down'] = pct_change.apply(lambda x: classify(x),axis = 1)
+    pct_change['Weekday'] = pct_change.index.map(lambda x: x.isoweekday())
+    count = pct_change.groupby(['Weekday', 'Up/Down']).size()
+    print(count)
+    break
+
